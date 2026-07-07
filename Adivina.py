4835f8caa15c5f72d25f864d4af41bd6a4016603
mteo <cortinamateo211@gmail.com>
2026-07-07 08:33:47 -0500

    Pruebas
    -Complete parte del codigo
    -quite los comentarios
    -agregue main con su ifmain

--- Adivina.py
+++ Adivina.py
@@ -1,6 +1,3 @@
-# un juego que dnd hay q adivinar el numero que salio
-# si el numero q das es menor -> decir que un poco mayor / si es menor -> un poco menor
-# si la diferencia es mas grande -> dar otro mensaje
 import random
 
 
@@ -9,13 +6,48 @@ class Game:
         self.numeroRandom = random.randint(1, 10)
 
     def Juego(self, numero):
-        diferencia = numero - self.numeroRandom
+        while True:
+            diferencia = abs(numero - self.numeroRandom)
 
-        if numero == self.numeroRandom:
-            print("!!Correcto")
-            return
-        elif numero < self.numeroRandom:
-            if diferencia <= 2:
-                print("Muy cerca, solo un poco mas alto")
-            else:
-                print("Un numero un poco mas alto")
+            if numero == self.numeroRandom:
+                print("!!Correcto")
+                return
+            elif numero < self.numeroRandom:
+                if diferencia <= 2:
+                    print("Muy cerca, solo un poco mas alto")
+                else:
+                    print("Un numero un poco mas alto")
+            elif numero > self.numeroRandom:
+                if diferencia <= 2:
+                    print("Muy cerca, solo un poco mas bajo")
+                else:
+                    print("Un numero un poco mas bajo")
+
+    def Pedirnumero(self):
+        valor = int(input("Dame un numero: "))
+        return valor
+
+    def Usuario(self):
+        self.Juego(self.Pedirnumero())
+
+
+def main():  # El menu principal
+    gg = Game()
+
+    while True:
+        print("\n" + "=" * 60)
+        print("1. Iniciar")
+        print("2. Salir")
+        elige = input("Elige: ")
+
+        if elige == "2":
+            print("Saliendo...")
+            break
+        elif elige == "1":
+            gg.Usuario()
+        else:
+            print("Ingrese un valor válido")
+
+
+if __name__ == "__main__":
+    main()
