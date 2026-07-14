a34f7a014c783d77b99713dc6cf5c2b9c71a26f2
mteo <cortinamateo211@gmail.com>
2026-07-14 10:17:49 -0500

    docs: comentarios
    - se le agregaron comentarios para hacer mas entendible el codigo

--- Adivina.py
+++ Adivina.py
@@ -1,47 +1,49 @@
+# importan librerias
 import random
 
 
 class Game:
     def __init__(self):
-        self.numeroRandom = random.randint(1, 20)
+        self.numeroRandom = random.randint(1, 20)  # genera un numero random entre 1-20
 
     def Juego(self):
         while True:
             try:
-                numero = self.Pedirnumero()
+                numero = self.Pedirnumero()  # pide un numero al Usuario
+                # que tan cerca del numero ersta el valor que dio el Usuario:
                 diferencia = abs(numero - self.numeroRandom)
 
-                if numero == self.numeroRandom:
+                if numero == self.numeroRandom:  # si le atino
                     print("!!Correcto!!")
                     return
-                elif numero > 20 or numero < 1:
+                elif numero > 20 or numero < 1:  # si se sale del los parametros
                     print("Ingresa un valor dentro de los parametros")
 
-                elif numero < self.numeroRandom:
-                    if diferencia <= 5:
+                elif numero < self.numeroRandom:  # si puso un numero bajo
+                    if diferencia <= 5:  # si esta cerca por 5 numeros
                         print("Muy cerca, solo un poco mas alto")
-                    else:
+                    else:  # si no esta cerca
                         print("Un numero un poco mas alto")
-                elif numero > self.numeroRandom:
-                    if diferencia <= 5:
+                elif numero > self.numeroRandom:  # si puso un numero alto
+                    if diferencia <= 5:  # si esta cerca por 5 numeros
                         print("Muy cerca, solo un poco mas bajo")
-                    else:
+                    else:  # si no esta cerca
                         print("Un numero un poco mas bajo")
-            except ValueError:
+            except ValueError:  # en caso de error por numeros
                 print("Ingresa un valor entero, o valido")
-            except Exception as e:
+            except Exception as e:  # en caso de error:
                 print(f"Ingrese un valor valido\nError: {e}")
 
-    def Pedirnumero(self):
+    def Pedirnumero(self):  # pide numero al Usuario
         valor = int(input("Dame un numero del 1-20: "))
-        return valor
+        return valor  # devuelve el numero
 
-    def Usuario(self):
+    def Usuario(self):  # inicia el juego
         self.Juego()
 
 
 def main():  # El menu principal
-    gg = Game()
+    gg = Game()  # asigna variable a Game
 
     while True:
         print("\n" + "=" * 60)
@@ -58,5 +60,5 @@ def main():  # El menu principal
             print("Ingrese un valor válido")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # inicia el programa
     main()
